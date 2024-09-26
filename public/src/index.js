d242b42b9bcb7ef94c9eca0626ee9989d277bdb0
Ronov76 <aaron.hansch@gmx.de>
2024-09-26 23:57:10 +0200

    FrontEndErstellung

--- public/src/index.js
+++ public/src/index.js
@@ -1,30 +1,78 @@
 const express = require("express");
 const path = require("path");
-require("dotenv").config();
+const axios = require("axios");
 
 const app = express();
 const port = 3003;
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../website")));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../website/index.html'));
+});
+
+app.post("/nutzerErstellen", async (req, res) => {
+
+  const { id, name, alter } = req.body
+
+  if(!id) {
+    return res.status(400).json({error: "ID muss eingegeben werden"})
+  }
+  if(!name) {
+    return res.status(400).json({error: "Name muss eingegeben werden!"});
+  }
+  if(!alter) {
+    return res.status(400).json({error: "Alter muss eingegeben werden!"});
+  }
+
+  try{
+    
+    const response = await axios.post("http://nutzerloeschen:3001/nutzererstellen", { id });
+    const message = response.data.message;
+
+    if(message === "statusBool") {
+      return res.status(200).json({ message: "Nutzer konnte nicht erstellt werden!"})
+    }
+
+  } catch (error) {
+    console.error("Fehler bei der Anfrage:", error.message);
+    return res.status(500).send("Interner Serverfehler");
+  }
+
+  return res.status(200).json({ message: "Nutzer erfolgreich erstellt"});
+
+});
 
 app.post("/nutzerLoeschen", async (req, res) => {
 
   const { id } = req.body
+  
+  console.log(req.body);
 
   if(!id) {
     return res.status(400).json({error: "ID muss eingegeben werden"})
   }
 
-  const response = await axios.post("http://nutzerloeschen:3002/nutzerloeschen", { id });
-  
-  return res.status(response.status)
+  try{
+    
+    const response = await axios.post("http://nutzerloeschen:3002/nutzerloeschen", { id });
+    const message = response.data.message;
 
-});
+    if(message === "statusBool") {
+      return res.status(200).json({ message: "Nutzer konnte nicht geloescht werden!"})
+    }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  } catch (error) {
+    console.error("Fehler bei der Anfrage:", error.message);
+    return res.status(500).send("Interner Serverfehler");
+  }
+
+  return res.status(200).json({ message: "Nutzer erfolgreich gelöscht"});
+
 });
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`);
 });
+
---
+++ public/website/index.js
@@ -0,0 +1,81 @@
+
+function nutzerErstellen() {
+    waiting();
+
+    const id = parseInt(document.getElementById("txtID").value, 10);
+    const name = document.getElementById("txtName").value;
+    const alter = parseInt(document.getElementById("txtAlter").value, 10);
+
+    fetch("http://localhost:3003/nutzerstellen", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: id,
+            name: name,
+            alter: alter
+        })
+    })
+    .then(response => {
+        if (response.ok) {
+            document.getElementById("status").textContent = "Nutzer wurde erfolgreich erstellt";
+            return response.json();
+        } else {
+            document.getElementById("status").textContent = "Fehlschlag bei der Erstellung";
+            return response.json();
+        }
+    })
+    .catch(error => {
+        console.error("Fehler: ", error);
+        document.getElementById("status").textContent = "Fehlschlag bei der Erstellung";
+    })
+
+    clear();
+
+}
+
+function nutzerLoeschen() {
+    waiting();
+
+    const id = parseInt(document.getElementById("txtID").value, 10);
+
+    fetch("http://localhost:3003/nutzerloeschen", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: id
+        })
+    })
+    .then(response => {
+        if (response.ok) {
+            document.getElementById("status").textContent = "Nutzer wurde erfolgreich geloscht";
+            return response.json();
+        } else {
+            document.getElementById("status").textContent = "Fehlschlag bei der Loeschung";
+            return response.json();
+        }
+    })
+    .catch(error => {
+        console.error("Fehler: ", error);
+        document.getElementById("status").textContent = "Fehlschlag bei der Loeschung.";
+    })  
+
+    clear();
+
+}
+
+
+
+
+function clear(){
+    document.getElementById("txtID").value = 0
+    document.getElementById("txtName").value = ""
+    document.getElementById("txtAlter").value = 0
+}
+
+function waiting() {
+    document.getElementById("status").textContent = "In Bearbeitung"
+}
